6e03ea38df44b9edb44cfc207f7124b202c9ef77
Lukas Mateffy <github@mateffy.me>
2021-09-01 04:00:41 +0200

    ok

--- packages/mission-control/app/auth/types.js
+++
@@ -1,9 +0,0 @@
-/**
- * User object
- * @typedef {express.Router} User
- * @type {object}
- * @property {string} username Username
- * @property {string} password Password (hashed)
- * @property {string} avatarUrl URL to the avatar
- * @property {string} displayName The name but nice
- */
